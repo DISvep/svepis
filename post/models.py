9cdad61d3f08343bc379cf5405ff33942257df6e
DISvep <321861bro@gmail.com>
2025-01-12 12:33:20 +0200

    post adding

--- post/models.py
+++ post/models.py
@@ -4,14 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     date = models.DateTimeField(auto_now_add=True)
 
 
 class Announcement(models.Model):
-    title = models.CharField(max_length=255)
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='announcements')
     date = models.DateTimeField(auto_now_add=True)
--- post/views.py
+++ post/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import DetailView, ListView
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import DetailView, ListView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -15,5 +17,28 @@ class PostList(ListView):
     template_name = 'post_list.html'
     context_object_name = 'posts'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['PostForm'] = PostForm
+        
+        return context
     
+    def get_queryset(self):
+        return Post.objects.all().order_by('-date')
+    
+
+class CreatePost(CreateView):
+    model = Post
+    
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+            return HttpResponseRedirect(reverse('post-list'))
+        else:
+            return HttpResponseRedirect(reverse('post-list'))
 
