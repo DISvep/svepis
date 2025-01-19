3c23bb2066852fe439fbaf2080e07048330023a9
DISvep <321861bro@gmail.com>
2025-01-19 09:47:23 +0200

    comment

--- comment/urls.py
+++ comment/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import PostCommentView, CreatePostCommentView
+from .views import PostCommentView, CreatePostCommentView, UpdatePostCommentView, DeletePostCommentView
 
 
 urlpatterns = [
     path('<int:pk>/', PostCommentView.as_view(), name='post-comments'),
-    path('<int:pk>/create', CreatePostCommentView.as_view(), name='comment-create')
+    path('<int:pk>/create', CreatePostCommentView.as_view(), name='comment-create'),
+    path('<int:pk>/update', UpdatePostCommentView.as_view(), name='comment-update'),
+    path('<int:pk>/delete', DeletePostCommentView.as_view(), name='comment-delete')
 ]
