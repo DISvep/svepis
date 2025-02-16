a79669eb0d895e8dc2d3ffecb85a922b873e4ac3
DISvep <321861bro@gmail.com>
2025-02-16 21:22:22 +0200

    superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,4 +1,6 @@
+from subscription.models import FriendList, SubscriptionList
 from django.contrib.auth.models import User
+from portal.models import Portal
 import os
 
 username = os.getenv("DJANGO_SUPERUSER_USERNAME")
@@ -6,7 +8,10 @@ email = os.getenv("DJANGO_SUPERUSER_EMAIL")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+    superuser = User.objects.create_superuser(username=username, email=email, password=password)
+    Portal.objects.create(user=superuser)
+    FriendList.objects.create(user=superuser)
+    SubscriptionList.objects.create(user=superuser)
     print("Superuser created")
 else:
     print("Superuser already exists")
