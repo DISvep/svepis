efc8014cca9bba16262d6556c862ee4e86d7ba2e
DISvep <102363179+DISvep@users.noreply.github.com>
2025-02-16 14:33:12 +0200

    Update settings.py

--- svepis/settings.py
+++ svepis/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+0$)ntt0li%@@+rr%xlad#znq&396q8=fch_b!^5wdr&lfrv*a'
+SECRET_KEY = os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = [os.environ.get('allowed_hosts', '127.0.0.1')]
 
 
 # Application definition
