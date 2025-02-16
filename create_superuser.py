7abdfe6cfc90577c322ffc96f59ae2fd4f62560c
DISvep <321861bro@gmail.com>
2025-02-16 19:32:13 +0200

    superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 import os
 
-User = get_user_model()
 username = os.getenv("DJANGO_SUPERUSER_USERNAME")
 email = os.getenv("DJANGO_SUPERUSER_EMAIL")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
