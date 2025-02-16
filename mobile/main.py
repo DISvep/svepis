988e9cf139002a6a5211ebcaac8a9186974bd96f
DISvep <102363179+DISvep@users.noreply.github.com>
2025-02-16 14:23:07 +0200

    Update main.py

--- mobile/main.py
+++ mobile/main.py
@@ -1,27 +1,27 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-import requests
+# from kivy.app import App
+# from kivy.uix.button import Button
+# from kivy.uix.boxlayout import BoxLayout
+# import requests
 
 
-class NetworkApp(App):
-    def answer(self):
-        response = requests.get('http://127.0.0.1:8000/post/api/posts/')
+# class NetworkApp(App):
+#     def answer(self):
+#         response = requests.get('http://127.0.0.1:8000/post/api/posts/')
         
-        if response.status_code == 200:
-            response = response.json()
+#         if response.status_code == 200:
+#             response = response.json()
             
-            print(response)
-        else:
-            print('all bad')
+#             print(response)
+#         else:
+#             print('all bad')
     
-    def build(self):
-        button = Button(text='hello world')
-        button.on_press = self.answer
+#     def build(self):
+#         button = Button(text='hello world')
+#         button.on_press = self.answer
         
-        return button
+#         return button
     
     
-app = NetworkApp()
-app.run()
+# app = NetworkApp()
+# app.run()
     
