473c413948663e511d3e69ba6212113c6bc40f8b
DISvep <102363179+DISvep@users.noreply.github.com>
2025-02-22 23:18:31 +0200

    Update asgi.py

--- svepis/asgi.py
+++ svepis/asgi.py
@@ -7,16 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svepis.settings')
+django.setup()
+
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from chat import routing
-import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svepis.settings')
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
