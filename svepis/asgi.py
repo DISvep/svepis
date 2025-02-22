1a741dfe197c41217446da0857131da94d49437a
DISvep <321861bro@gmail.com>
2025-02-22 17:05:31 +0200

    trying to fix sockets

--- svepis/asgi.py
+++ svepis/asgi.py
@@ -8,21 +8,29 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from chat import routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'svepis.settings')
+django.setup()
+
+
+from django.core.asgi import get_asgi_application
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chat import routing
+
 
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(
-            URLRouter(
-                routing.websocket_urlpatterns
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    routing.websocket_urlpatterns
+                )
             )
         )
     }
