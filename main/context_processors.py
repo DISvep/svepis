2a07ce71fe24512b1839bd67bbc8f8e7c3b99c8a
DISvep <102363179+DISvep@users.noreply.github.com>
2025-02-23 14:49:44 +0200

    Update context_processors.py

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,6 +3,6 @@
 def current_user_context(request):
     return {
         'current_user': request.user,
-        'message_notification': request.user.message_notifications.filter(is_read=False).count()
+        # 'message_notification': request.user.message_notifications.filter(is_read=False).count()
     }
 
